6ea2184db0353f7afcfa99fa0f44cc956ce526d7
mouridesadikh <mouride987@gmail.com>
2020-10-20 15:49:55 +0100

    my commit

--- resources/views/servicesPages/addService.blade.php
+++ resources/views/servicesPages/addService.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.Layout')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/print.css') }}" media="print">
 <div class="wrapper theme-1-active navbar-top-light">
 	<!-- Main Content -->
 	<div class="page-wrapper">
@@ -90,7 +91,7 @@
 									<!-- <form> -->
 										<div class="row mor1">
 											<div class="col-sm-3">Produit</div>
-											<div class="col-sm-3">Taille</div>
+											<div class="col-sm-3">Taille/Marque</div>
 											<div class="col-sm-3">Montant</div>
 											<div class="col-sm-3">Action</div>
 										</div>
@@ -119,7 +120,7 @@
 										<div class="row">
 											<div class="col-sm-12 mt-20">
 												<div class="button-box"> 
-													<a id="ticket" class="btn btn-primary btn-rounded btn-block" href="#" alt="default" data-toggle="modal" data-target="#responsive-modal-ticket">Imprimer Ticket</a> 
+													<a id="ticket" class="btn btn-primary btn-rounded btn-block" href="#" alt="default" data-toggle="modal" data-target="#responsive-modal-ticket">Voir Ticket</a> 
 												</div>
 											</div>
 										</div>
@@ -142,20 +143,38 @@
 								<div class="form-wrap">
 									<!-- <form> -->
 									<div class="row">
-										<div class="col-sm-12">
-											<label class="control-label mb-10">Produit*</label>
+										<div class="col-sm-2" ></div>
+										<div class="col-sm-4" >
+										
+										
+												<button class="btn" id="tapi" style="border: 2px solid" onclick="produit(1,'tapi')">
+												  <img  style="width : 65px;" src="{{asset('img/tapi2.png')}}"/>
+												</button>
+											 
+											
+										
+											
+											
+											{{-- <label class="control-label mb-10">Produit*</label>
 											<div class="form-group">
 												<select class="selectpicker" name="produit" id="produit" data-style="btn-warning btn-outline">
 													@foreach($produits as $produit)
 														<option value="{{ $produit->id }}">{{ $produit->nomProduit }}</option>
 													@endforeach
 												</select>
-											</div>
+											</div> --}}
 										</div>
+										<div class="col-sm-4">
+											<button class="btn" id="auto" style="border: 2px solid" onclick="produit(2,'auto')">
+												<img  style="width : 65px;" src="{{asset('img/auto.jpg')}}"/>
+											  </button>
+											  <br><br>
+										</div>
+										<div class="col-sm-2" ></div>
 										<div class="col-sm-12">
 											<div class="form-group">
-												<label class="control-label mb-10">Taille*</label>
-												<input type="text" id="taille" name="taille" class="form-control" placeholder="La taille">
+												<label class="control-label mb-10">Taille/Marque</label>
+												<input type="text" id="taille" name="taille" class="form-control" placeholder="La taille / marque">
 											</div>
 										</div>
 										<div class="col-sm-12">
@@ -212,43 +231,30 @@
 
 			<!-- Row Gestion Modal Ticket--->
 			<!-- /.modal -->
-			<div id="responsive-modal-ticket" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+			<div id="responsive-modal-ticket" class="container modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
 				<div class="modal-dialog">
-					<div class="modal-content col-md-5">
+					<div class="modal-content col-md-7">
 						<div class="modal-header">
 							<!-- <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button> -->
-							<h5 class="modal-title">SENLAVAGE</h5>
+							<p style="text-align: center;text-decoration-style:solid" class="modal-title">SENLAVAGE</p>
 						</div>
-						<div class="modal-body">
-							<form>
-								<div class="row mb-20  ticket">
-									<div class="col-md-4">produits</div>
-									<div class="col-md-4">Taille</div>
-									<div class="col-md-4">Prix</div>
-								</div>
-								<div class="row" id="ticket">
-									
-								</div>
-								<div class="row" >
-									<div class="col-md-4">Tapis</div>
-									<div class="col-md-4"></div>
-									<div class="col-md-4">2000f</div>
-								</div>
-								<div class="row">
-									<div class="col-md-4">Voiture</div>
-									<div class="col-md-4"></div>
-									<div class="col-md-4">5000f</div>
-								</div>
-								<div class="row mt-20">
-									<div class="col-md-4">Total</div>
-									<div class="col-md-4"></div>
-									<div class="col-md-4">2000f</div>
-								</div>
+						<div class="modal-body" id="tticket" >
+							<table class="table" >
+
+								<thead>
+									<th>Produit</th>
+									<th>Taille/Marque</th>
+									<th>Prix</th>
+								</thead>
+								<tbody id="ticket_imprimer">
+                                            
+								</tbody>
+							
 								
-							</form>
+							</table>
 						</div>
 						<div class="modal-footer">
-							<button type="button" class="btn btn-default btn-xs btn-icon left-icon" data-dismiss="modal">Imprimer</button>
+							<button type="button" id="print_ticket" class="btn btn-default btn-xs btn-icon left-icon" data-dismiss="modal">Imprimer</button>
 							<!-- <button type="button" class="btn btn-success btn-xs" data-dismiss="modal">Imprimer</button> -->
 						</div>
 					</div>
@@ -262,33 +268,46 @@
 @endsection
 @section('scripts')
 <script>
+    var tabs = [];
+	var $n = 1;
+	var $produit = 1 ;
 	$(document).ready(function(){
-		$('#btnButton').hide();
-		$('#total').hide();
-		$('#ticket').hide();
-		var tabs = [];
-		var $n = 1;
-		$cli = $('#ajoutPanier').click(function(){
-	
-			$produit =  $('#produit').val();
-			if($produit==1){
-				$vall = "Tapis";
-			}else{
-				$vall = "Voiture";
-			}
-			$taille  =  $('#taille').val();
-			$montant =  Number($('#montant').val());
-			$somme   =  Number($('#somme').text());
-			$laSomme =  $somme + $montant;
-	
-			$('#somme').text($laSomme);
-			$('#panier').append("<div id='div-"+$n+"'><div class='col-sm-3'>"+$vall+"</div><div class='col-sm-3'>"+$taille+"</div><div class='col-sm-3'>"+$montant+"</div><div class='col-sm-3'>"+"<a href='javascript:void(0)' title='Update'><i class='ti-pencil txt-primary'></i></a>"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+"<a  id='div-"+$n+"' title='Delete'  onclick='document.getElementById('div-"+$n+"').remove();'><i class='zmdi zmdi-delete txt-danger'></i></a>"+"</div></div>");
-			console.log('div-'+$n);
-			tabs1 = new Array($produit,$taille,$montant);
-			console.log(tabs1);
-			tabs.push(tabs1);
+		loadPanier();
+	});
+	function supprimer(id){
+			console.log(this.tabs)
+			tabs.splice(id,1);
 			console.log(tabs);
+			loadPanier();
+       }
+	function  produit(id,productId){
+		 
+         $produit = id;
+		 console.log(productId);
+		 if(productId == 'tapi')
+		 {
+			$("#tapi").css({'border': '3px solid #487f9c','zoom':'1'});
+			$("#auto").css({'border': '2px solid'});
+		 }else{
+			$("#tapi").css({'border': '2px solid'});
+			$("#auto").css({'border': '3px solid #487f9c','zoom':'1'});
+		 }
+		 
+	}
 
+   function loadPanier(){
+
+		    $('#btnButton').hide();
+			$('#total').hide();
+			$('#ticket').hide();
+			$('#panier').html("");
+            var somme = 0;
+			tabs.forEach((element,index) => {
+				$('#panier').append("<div id='div-"+$n+"'><div class='col-sm-3'>"+element[1]+"</div><div class='col-sm-3'>"+element[2]+"</div><div class='col-sm-3'>"+element[3]+"</div><div class='col-sm-3'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+"<a  id='"+index+"' title='Delete' onclick='supprimer("+index+");'><i class='zmdi zmdi-delete txt-danger'></i></a>"+"</div></div>");
+				somme = somme + element[3];
+				
+			});
+            $('#somme').text(somme);
 			$('#btnButton').show();
 			$('#total').show();
 			$('#ticket').show();
@@ -296,11 +315,55 @@
 			$('#taille').val("");
 			$('#montant').val("");
 			$n++;
-		});
+   }
+  
+		$('#ajoutPanier').click(function(){
+	
+			//$produit =  $('#produit').val();
+			if($produit==1){
+				$vall = "Tapis";
+			}else{
+				$vall = "Voiture";
+			}
+			$taille  =  $('#taille').val();
+		    $montant =  Number($('#montant').val());
+		
+			console.log('div-'+$n);
+			tabs1 = new Array($produit,$vall,$taille,$montant);
+			console.log(tabs1);
+			tabs.push(tabs1);
+			console.log(tabs);
+			loadPanier();
+        });
+ 
+
+	    
 		$('#ajouterStatut').click(function(){
 			$statut = $('#statut').val();
 			// console.log($statut);
 			$('#ticket').html() = $('#panier').html();
+		});
+		$('#print_ticket').click(function(){
+			   var mywindow = window.open('', 'PRINT', '');
+			   mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+               mywindow.document.write('</head><body >');
+			   mywindow.document.write(document.getElementById('tticket').innerHTML);
+			   mywindow.document.write('</body></html>');
+			   mywindow.document.close();
+			   mywindow.focus();
+               mywindow.print();
+			   //mywindow.close();
+             
+		});
+		$('#ticket').click(function(){
+			 var html = "";
+			 var total = 0;
+			 tabs.forEach(element=>{
+               html = html + "<tr><td>"+element[1]+"</td><td>"+element[2]+"</td><td>"+element[3]+"</td></tr>";
+			   total = total + element[3];
+			 });
+			 html = html + "<tr><td>Total :</td><td > </td><td>"+total+" Fcfa</td></tr>"
+			$('#ticket_imprimer').html(html);
 		});
 		$("div-"+$n+"").click(function(){
 			console.log("Dokhna");
@@ -325,6 +388,6 @@
 			});
 		});
 		
-});
+
 </script>
 @endsection
